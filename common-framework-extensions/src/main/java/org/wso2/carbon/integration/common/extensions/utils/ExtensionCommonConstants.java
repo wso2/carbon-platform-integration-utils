af37195e6d9019c5799f07f9fc5f391c1a216a72
sanethd <sanethd@wso2.com>
2014-12-11 09:36:16 +0530

    Incorporating review comments

--- common-framework-extensions/src/main/java/org/wso2/carbon/integration/common/extensions/utils/ExtensionCommonConstants.java
+++ common-framework-extensions/src/main/java/org/wso2/carbon/integration/common/extensions/utils/ExtensionCommonConstants.java
@@ -51,6 +51,6 @@ public class ExtensionCommonConstants {
     public static final String OPERA_BROWSER = "opera";
     public static final String DATE_FORMAT_YY_MM_DD_HH_MIN_SS = "yyyy_MM_dd_HH_mm_ss";
     public static final String UNDERSCORE = "_";
-    public static final String SCREEN_SHOT_LOCATION="SeleniumImageFiles/FailedTest";
-    public static final String SCREEN_SHOT_EXTENSION=".png";
+    public static final String SCREEN_SHOT_LOCATION = "capturedscreens/failedtests";
+    public static final String SCREEN_SHOT_EXTENSION = ".png";
 }
